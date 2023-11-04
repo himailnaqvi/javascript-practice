66d72389ef962f5da1f6a1ff08b82dfdf83466d5
himailnaqvi <148746970+himailnaqvi@users.noreply.github.com>
2023-11-04 10:50:18 +0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,94 @@
+
+
+// arrays
+
+// var city1="karachi";
+// var city2="lahore";
+// var city3="multan";
+// var city4="quetta";
+// var city5="islamabad";
+// var city6="welcome to"+  " "  + city5
+// console.log(city6)
+
+
+// var cities=["chicago","paris","washington","denver","los angeles","atlanta"];
+// console.log(cities[4]+ " " +cities[3])
+
+// pop
+
+// var arr=["lizard","cat","snake","dog","crocodile","camel"];
+// arr.pop()
+// console.log(arr)
+
+
+// var animals=["monkey","dianasor","tiger"];
+// animals.pop()
+// console.log(animals)
+
+
+// var cousins=["haya","abeha","sidra"]
+// cousins.pop()
+// console.log(cousins)
+
+// push
+
+// var colours=["blue","purple","red","green","orange"];
+// colours.push("pink");
+// console.log(colours)
+
+
+// var mobiles=["techno","vivo","samsung"];
+// mobiles.push("iphone");
+// console.log(mobiles)
+
+
+// var creams=["ponds","nivea","fair and lovely"];
+// creams.push("bb cream","face powder");
+// console.log(creams)
+
+
+// shift
+
+// var tea=["water","sugar","milk"];
+// tea.shift();
+// console.log(tea)
+
+// var commerce=["accounting","poc","economics"];
+// commerce.shift();
+// console.log(commerce)
+
+// unshift
+
+// var animals=["dog","cat","lizard"];
+// animals.unshift("lion","tiger")
+// console.log(animals)
+
+
+// var tea=["water","tapal danedar"];
+// tea.unshift("milk","sugar");
+// console.log(tea)
+
+
+// var pets=["dog","cat"];
+// pets.unshift("parrot","rabbit");
+// console.log(pets)
+
+// splice
+
+// var pet1=["dog","cat","imu","ox","fly"];
+// pet1.splice(2,0,"penguin")
+// pet1.splice(2,2,"lamb")
+// console.log(pet1)
+
+// slice
+
+// var arr=[1,2,3,4,5,6];
+// var arr2=arr.slice(0,3);
+// var arr3=arr.slice(0)
+// console.log(arr3)
+
+// var arrnew=["jeans","narrow","balbottom","white pant","blue jeans"];
+// var arrnew2=arrnew.slice(0,3);
+// console.log(arrnew2)
+
+
---
+++ loops.js
@@ -0,0 +1,161 @@
+
+// var cities=["karachi","lahore","islamabad","peshawar","quetta","rawalpindi"]
+// for(let i=0; i<=cities.length-1; i++){
+// cities[i]=cities[i] + "city"
+// console.log(cities)
+// }
+
+// var table= +prompt("enter table number");
+// var length= +prompt("enter the length of table");
+
+// for (let i=1; i<length; i++){
+//     document.write(table+"x"+ i +"="+(table*i));
+//     document.write("<br>");
+// }
+
+// for (let i=1; i<100; i=i+2){
+//     console.log(i)
+// }
+
+// let num=[];
+// for(let i = 1;i<=10;i++){
+//     num.push(i)
+// }
+// console.log=[num];
+
+// let myWork=[];
+
+// for (let i=1; i<10;i++){
+// myWork.push ("lesson "+i);
+// }
+//     console.log(myWork)
+// let i=5;i<=50;i+5)
+// console.log
+// for (g(i)
+
+
+// var_name="";
+// var myClassNum="";
+// var mk=10;
+// var=$abbb='';
+
+
+
+// var cities=["dubai","chicaho","mexico","houston","ny"];
+// // cities[0]
+// var cityToSearch="karachi";
+// for(let i=0;i<cities.length-1;i++)
+// {
+//     if(cities[i]===cityToSearch){
+//         alert("city karachi is clean city")
+//         break;
+//     }else{
+//         alert("karachi not found")
+//     }
+// }
+
+
+// var arr=[2,3,4,6,8];
+
+// var city=[];
+// var cityToSearch="karachi";
+// var found=false;
+
+
+// for(i=0; i>5;i++);{
+
+
+// jb kisi bhi number sa multiply krna hoga aisa hi hoga keep this in your mind
+
+// for(let i=1;i<=70 ; i++){
+//     if (i % 11 ===0){
+//         console.log(i)
+//     }
+// }
+
+// var originalNumber=[3];
+// var numberAdded=[34];
+// var newNumber=[originalNumber-numberAdded];
+// console.log(newNumber)
+
+
+
+// practice of javascript
+
+// 
+// Calculate the length of a given array
+
+// var num =[1,3,"a","b","f","a"]
+// const arr=[1,2,34,4,5,5];
+// let count=0;
+// for(let i=0;i<arr.length;i++){                           for const sa bhi ho sakta hai or upar wala jo commit out hai us sa bhi
+//     count++;
+    // count=count+1
+    // count +=count {yeh teeno ek hi kam krta hain}
+// for(const item in arr) count++
+// // }
+
+// console.log("the elements in array are total"+count)
+
+
+
+//calculate how many times "a "has appeared
+// var arr=[1,3,"a","b","f","a"]
+// var count=0;
+// calculateA=["a",2,4,"b","a"];
+// var count=0;
+// for(let i=0;i<=calculateA.length-1;i++){
+//     if(calculateA[i]=== 'a'){
+//         count++;
+//     }
+// }
+// console.log("'a'appeared" +count +"times")
+
+
+
+// find is there any element which is Number.
+
+
+
+
+
+// print all the names in the given list of students and greet them good morning.
+
+// var students=["ali","zain","asad","ayan","raza"];
+// var greeting="good morning"
+
+// for (let i=0; i<=students.length; i++){
+//      console.log(students [i] + " "+greeting)
+// }
+
+
+
+// add all the elements and show the count
+// const arr=[2,4,6,8,10];
+// let count =0;
+// for (let i=0; i<= arr.length-1;i++){
+//     count=count+arr[i]
+
+// }
+// console.log(count)
+
+
+
+// make array of prices n increase every price by 10%
+// const price=[100,300,600];
+// const newPrice=[];
+// for(let i=0;i<price.length;i++){
+// newPrice[i]=(0.1)*(price[i])+(price[i]);
+// console.log("the new price is"+newPrice[i])
+// }
+
+
+
+
+
+
+
+
+
+
+
